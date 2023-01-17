97ecc388720e64fe0898c63373fcf90f27eb858c
Joao Bione <joaph@ciandt.com>
2023-01-16 22:41:49 -0300

    test: test OX vs windows regex pattern

--- Gemfile
+++ Gemfile
@@ -4,3 +4,5 @@ source "https://rubygems.org"
 
 # gem "rails"
 gem "rspec"
+gem 'rubocop', group: 'development'
+gem 'rubocop-rspec', group: 'development'
---
+++ main.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+# items = []
+# 	File.foreach('games.log').slice_before(/ShutdownGame/).map do |game_lines|
+# 		game_lines.select { |line| line.match(/Item/) }.map do |line|
+# 			match = line.match(/(?<timestamp>\S+).*Item: (?<player_id>\S+) (?<weapon>\S+)/)
+# 			items << match.named_captures
+# 		end.compact
+# 	end
+# puts items
+#-{60}/
+match_kill_data = []
+games = {}
+game_count = 0
+File.foreach('test.log').slice_after(/InitGame/).slice_before(/ShutdownGame/ || /-{60}/)
+    .map do |game_lines|
+  game_lines.select do |line|
+    line.map do |line|
+      if line.match(/InitGame/)
+        game_count += 1
+        games[game_count] = { total_kills: 0, players: [], kills: {} }
+        elsif line.match(/(?<timestamp>\S+).*Kill: (?<killer_id>\S+) (?<killed_id>\S+) (?<means_of_death_id>\d+): (?<killer_name>\S+) killed (?<victim_name>.+) by (?<means_of_death>\S+)/)
+          match = line.match(/(?<timestamp>\S+).*Kill: (?<killer_id>\S+) (?<killed_id>\S+) (?<means_of_death_id>\d+): (?<killer_name>\S+) killed (?<victim_name>.+) by (?<means_of_death>\S+)/)
+          games[game_count][:total_kills] += 1
+          if !games[game_count][:players].include?(match[:killer_name]) && match[:killer_name] != '<world>'
+            games[game_count][:players] << match[:killer_name]
+            games[game_count][:kills][match[:killer_name]] = 0
+          end
+          games[game_count][:players] << match[:victim_name] unless games[game_count][:players].include?(match[:victim_name])
+          games[game_count][:kills][match[:killer_name]] =
+            games[game_count][:kills].key?(match[:killer_name]) ? (games[game_count][:kills][match[:killer_name]] + 1) : 0
+          games[game_count][:kills][match[:victim_name]] = 0 unless games[game_count][:kills].key?(match[:victim_name])
+        end
+    end.compact
+  end
+end
+#puts match_kill_data
+# puts game_count
+for n in games do puts n end
