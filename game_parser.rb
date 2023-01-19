caddcd7bc3f7a552b06e5a36eeb3de900656a511
Joao Bione <joaph@ciandt.com>
2023-01-19 16:58:58 -0300

    refactor: game_parser class refactored to better clean code and single responsiblity.

--- game_parser.rb
+++ game_parser.rb
@@ -6,25 +6,26 @@ class GameParser
   def initialize
     @games = {}
     @game_count = 0
-    @file = nil
   end
 
   def parse(file)
-    @file = File.open(file, 'r')
-    @file.each_line.slice_after(/InitGame/).slice_before(/ShutdownGame/ || /-{60}/).map do |game_lines|
-      game_lines.select do |line|
-        line.map do |line|
-          if line.match(/InitGame/)
-            @game_count += 1
-            @games[@game_count] = Game.new
-          elsif line.match(/(?<timestamp>\S+).*Kill: (?<killer_id>\S+) (?<killed_id>\S+) (?<means_of_death_id>\d+): (?<killer_name>\S+) killed (?<victim_name>.+) by (?<means_of_death>\S+)/)
-            @games[@game_count].process_kill(line)
-          end
-        end
+    File.open(file, 'r') do |file_opened|
+      file_opened.each_line.slice_after(/InitGame/).map do |game_lines|
+        process_game_lines(game_lines)
       end
     end
-    @file.close
     @games.reject! { |_game_id, game_data| game_data.players.empty? }
+  end
+
+  def process_game_lines(game_lines)
+    game_lines.select do |line|
+      if line.match(/InitGame/)
+        @game_count += 1
+        @games[@game_count] = Game.new
+      elsif line.match(/(?<timestamp>\S+).*Kill: (?<killer_id>\S+) (?<killed_id>\S+) (?<means_of_death_id>\d+): (?<killer_name>\S+) killed (?<victim_name>.+) by (?<means_of_death>\S+)/)
+        @games[@game_count].process_kill(line)
+      end
+    end
   end
 
   def write_games_report(file_name)
